01550822fa22e84408b6cdb0c7031205121f67d2
Nina <ninamarie315@gmail.com>
2014-09-05 17:27:23 -0400

    Inline works, wip list links

--- storyPassage.js
+++ storyPassage.js
@@ -1,31 +1,33 @@
 angular.module('storyPassage', [])
   .controller('PassageController', ['$scope', function($scope) {
-    var rawText = data.data[0].text;
-    //var linkTextRegExp = /\[\[([^)]+)\]\]/g;
-    //var linkToRegExp = /\|([^)]+)\]\]/;
-    var linkTextRegExp = /\[\[([^)]+)\|/;
-    var linkToRegExp = /\|([^)]+)\]\]/;
+    var rawText = data.data[1].text;
+    var linkTextRegExp = /\[\[([^)]+?)\]\]/g;
+    var linkToRegExp = /\|([^)]+?)\]\]/g;
 
-    $scope.linkText = linkTextRegExp.exec(rawText)[1];
-    $scope.linkTo = linkToRegExp.exec(rawText)[1];
-    //$scope.linkText = rawText.match(linkTextRegExp)[0];
+    $scope.trueRaw = data.data[0].text;
+
+    $scope.linkText = rawText.match(linkTextRegExp);
+    $scope.linkTo = rawText.match(linkToRegExp);
 
     var parsedText = rawText.replace("|" + $scope.linkTo, "").replace(/[\]}[{|]/g,'');
     $scope.raw = parsedText;
     //$scope.raw = rawText;
 
-    $scope.fillPassage = function(num) {
+    $scope.fillInlinePassage = function(num) {
+      //this works don't change it damn it
       num = typeof num !== 'undefined' ? num : 0;
 
+      var linkTextRegExp = /\[\[([^)]+)\|/;
+      var linkToRegExp = /\|([^)]+)\]\]/;
       rawText = data.data[num].text;
 
-      inline = true;
+      /*inline = true;
       if(inline){
         linkTextRegExp = /\[\[([^)]+)\|/;
         linkToRegExp = /\|([^)]+)\]\]/;
       } else {
 
-      }
+      }*/
 
       $scope.linkText = linkTextRegExp.exec(rawText)[1];
       $scope.linkTo = linkToRegExp.exec(rawText)[1];

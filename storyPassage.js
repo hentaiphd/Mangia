d7ac4a2232d734892fbe3ce327dd27a5b0cbee77
Nina <ninamarie315@gmail.com>
2014-09-06 12:58:42 -0400

    link list wip

--- storyPassage.js
+++ storyPassage.js
@@ -1,33 +1,42 @@
 angular.module('storyPassage', [])
   .controller('PassageController', ['$scope', function($scope) {
-    var rawText = data.data[1].text;
-    var linkTextRegExp = /\[\[([^)]+?)\]\]/g;
-    var linkToRegExp = /\|([^)]+?)\]\]/g;
+    console.log(data.data);
+    $scope.findPassage = function(num) {
+      for(i = 0; i < data.data.length; i++){
+        if(data.data[i].title == num){
+          return data.data[i].text;
+        }
+      }
+    };
 
-    $scope.trueRaw = data.data[0].text;
+    $scope.fillLinkListPassage = function(num) {
+      var rawText = $scope.findPassage(num);
 
-    $scope.linkText = rawText.match(linkTextRegExp);
-    $scope.linkTo = rawText.match(linkToRegExp);
+      //var rawText = data.data[num].text;
+      var linkTextRegExp = /\[\[([^)]+?)\]\]/g;
+      var linkToRegExp = /\|([^)]+?)\]\]/g;
 
-    var parsedText = rawText.replace("|" + $scope.linkTo, "").replace(/[\]}[{|]/g,'');
-    $scope.raw = parsedText;
-    //$scope.raw = rawText;
+      $scope.trueRaw = $scope.findPassage(num);
 
-    $scope.fillInlinePassage = function(num) {
+      $scope.linkTo = rawText.match(linkToRegExp);
+      $scope.linkText = rawText.match(linkTextRegExp);
+
+      for(i = 0; i < $scope.linkTo.length; i++){
+        $scope.linkTo[i] = $scope.linkTo[i].replace("|", "").replace(/[\]}[{|]/g,'');
+        $scope.linkText[i] = $scope.linkText[i].replace("|" + $scope.linkTo[i], "").replace(/[\]}[{|]/g,'');
+      }
+
+      var parsedText = rawText.replace(/\[\[([^)]+?)\]\]/g,'');
+      $scope.raw = parsedText;
+    };
+
+    $scope.fillInlineLinkPassage = function(num) {
       //this works don't change it damn it
       num = typeof num !== 'undefined' ? num : 0;
 
       var linkTextRegExp = /\[\[([^)]+)\|/;
       var linkToRegExp = /\|([^)]+)\]\]/;
       rawText = data.data[num].text;
-
-      /*inline = true;
-      if(inline){
-        linkTextRegExp = /\[\[([^)]+)\|/;
-        linkToRegExp = /\|([^)]+)\]\]/;
-      } else {
-
-      }*/
 
       $scope.linkText = linkTextRegExp.exec(rawText)[1];
       $scope.linkTo = linkToRegExp.exec(rawText)[1];

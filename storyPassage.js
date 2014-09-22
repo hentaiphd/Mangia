db274a93211711715d47a082fe7cdc5eb78d782a
hentaiphd <ninamarie315@gmail.com>
2014-09-22 15:24:53 -0400

    wow tag system is working

--- storyPassage.js
+++ storyPassage.js
@@ -1,26 +1,65 @@
-angular.module('storyPassage', [])
-  .controller('PassageController', ['$scope', function($scope) {
+angular.module('storyPassage', ['ngSanitize'], function($compileProvider) {
+    // configure new 'compile' directive by passing a directive
+    // factory function. The factory function injects the '$compile'
+    $compileProvider.directive('compile', function($compile) {
+      // directive factory creates a link function
+      return function(scope, element, attrs) {
+        scope.$watch(
+          function(scope) {
+             // watch the 'compile' expression for changes
+            return scope.$eval(attrs.compile);
+          },
+          function(value) {
+            // when the 'compile' expression changes
+            // assign it into the current DOM
+            element.html(value);
+
+            // compile the new DOM and link it to the current
+            // scope.
+            // NOTE: we only compile .childNodes so that
+            // we don't get into infinite loop compiling ourselves
+            $compile(element.contents())(scope);
+          }
+        );
+      };
+    });
+  })
+  .controller('PassageController', ['$scope', '$sce', function($scope, $sce) {
     $scope.findPassage = function(num) {
       for(i = 0; i < data.data.length; i++){
-        console.log(num);
         if(data.data[i].title == num){
-          return data.data[i].text;
+          return data.data[i];
         }
       }
     };
 
     $scope.fillLinkListPassage = function(newnum) {
       num = typeof newnum !== 'undefined' ? newnum : 0;
-      var rawText = $scope.findPassage(num);
+      var rawObj = $scope.findPassage(num);
+      var rawText = $scope.findPassage(num).text;
+      //different types of passages
       $scope.img_psg = false;
-      $scope.text_psg = false;
-      if(rawText.indexOf("img") > -1) {
-        $scope.img_psg = true;
+      $scope.list_psg = false;
+      $scope.inline_psg = false;
+
+      for(i = 0; i < rawObj.tags.length; i++){
+        if(rawObj.tags[i] == "list") {
+          $scope.list_psg = true;
+          console.log("list");
+        } else if(rawObj.tags[i] == "inline") {
+          $scope.inline_psg = true;
+          console.log("inline");
+        } else if(rawObj.tags[i] == "img") {
+          $scope.img_psg = true;
+          console.log("img");
+        }
+      }
+
+      if($scope.img_psg == true) {
         $scope.linkImg = rawText.split(",");
-        $scope.linkTo = $scope.linkImg[2];
-        $scope.linkText = [$scope.linkImg[1] + ".png"];
-      } else {
-        $scope.text_psg = true;
+        $scope.linkTo = $scope.linkImg[1];
+        $scope.linkText = [$scope.linkImg[0] + ".png"];
+      } else if($scope.list_psg == true){
         var linkTextRegExp = /\[\[([^)]+?)\]\]/g;
         var linkToRegExp = /\|([^)]+?)\]\]/g;
         $scope.linkTo = rawText.match(linkToRegExp);
@@ -30,28 +69,16 @@ angular.module('storyPassage', [])
           $scope.linkTo[i] = $scope.linkTo[i].replace("|", "").replace(/[\]}[{|]/g,'');
           $scope.linkText[i] = $scope.linkText[i].replace("|" + $scope.linkTo[i], "").replace(/[\]}[{|]/g,'');
         }
+      } else if($scope.inline_psg == true) {
+        var linkTextRegExp = /\[\[([^)]+)\|/;
+        var linkToRegExp = /\|([^)]+)\]\]/;
+
+        $scope.linkText = linkTextRegExp.exec(rawText)[1];
+        $scope.linkTo = linkToRegExp.exec(rawText)[1];
+
+        parsedText = rawText.replace("[[" + $scope.linkText + "|" + $scope.linkTo + "]]", "<a ng-click=\"fillLinkListPassage('" + $scope.linkTo + "')\" href='#'>" + $scope.linkText + "</a>").replace(/[\]}[{|]/g,'');
+
+        $scope.html = $sce.trustAsHtml(parsedText);
       }
-
-      $scope.trueRaw = $scope.findPassage(num);
-
-      var parsedText = rawText.replace(/\[\[([^)]+?)\]\]/g,'');
-      $scope.raw = parsedText;
-    };
-
-    $scope.fillInlineLinkPassage = function(num) {
-      //this works don't change it damn it
-      num = typeof num !== 'undefined' ? num : 0;
-
-      var linkTextRegExp = /\[\[([^)]+)\|/;
-      var linkToRegExp = /\|([^)]+)\]\]/;
-      rawText = data.data[num].text;
-
-      $scope.linkText = linkTextRegExp.exec(rawText)[1];
-      $scope.linkTo = linkToRegExp.exec(rawText)[1];
-
-      parsedText = rawText.replace("|" + $scope.linkTo, "").replace(/[\]}[{|]/g,'');
-      $scope.raw = parsedText;
-
-      //$scope.linky = $sce.trustAsHtml('<a ng-click="/$scope.linkTo" href="#"">hi</a>');
     };
   }]);
